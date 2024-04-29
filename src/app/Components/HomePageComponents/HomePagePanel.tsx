6b54860729b5bd5af54acdc9029e5477d32b4b59
xkolumber <lubosk158@gmail.com>
2024-04-29 22:39:28 +0200

    Homepage Panel

---
+++ src/app/Components/HomePageComponents/HomePagePanel.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React from "react";
+
+const HomePagePanel = () => {
+  return (
+    <div className="main_section">
+      <div className="flex flex-col md:flex-row md:gap-6">
+        <Image
+          src={"/panel.jpg"}
+          alt="panel"
+          width={500}
+          height={500}
+          className="w-full md:w-1/2"
+        />
+        <div
+          className="
+        "
+        >
+          <p>[Produkty]</p>
+          <h2>Stropný panel</h2>
+          <p>
+            Vďaka nízkej váhe a špeciálnemu spôsobu výroby sú dutinové panely
+            vhodné aj pre tie najnáročnejšie projekty. Veľkou výhodou našich
+            panelov je aj variabilita možností uloženia. Priestupy a otvory
+            podľa individuálnych požiadaviek môžeme pripraviť už pri výrobe,
+            vrátane otvorov pre schodiská a priestupy väčších rozmerov. Detaily
+            a technické riešenia sú pripravuje naše oddelenie projekcie, takže
+            náš zákazník vopred vie, ako bude jeho riešenie vyzerať. Dutinové
+            panely s hrúbkou 200 mm sa vyrábajú v troch rôznych pevnostných
+            kategóriách: základný model (200/1) zosilnený model (200/2) a
+            najpevnejší panel (200/3).
+          </p>
+          <button className="btn btn--secondary">Zistiť viac</button>
+          <p>Stropný panel</p>
+          <p>Stropný panel</p>
+          <p>Stropný panel</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePagePanel;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HomePageBestSolution from "./Components/HomePageComponents/HomePageBestSolution";
 import HomePageBuildFast from "./Components/HomePageComponents/HomePageBuildFast";
+import HomePagePanel from "./Components/HomePageComponents/HomePagePanel";
 
 export default function Home() {
   return (
     <main className="">
       <HomePageBestSolution />
       <HomePageBuildFast />
+      <HomePagePanel />
     </main>
   );
 }
