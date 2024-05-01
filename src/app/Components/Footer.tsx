dd05f106ca74a7cf6fb479f51f64e9baae04f8ef
xkolumber <lubosk158@gmail.com>
2024-05-01 22:45:31 +0200

    Footer element

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,33 +1,52 @@
 import Image from "next/image";
 import React from "react";
+import IconFooterFacebook from "./Icons/IconFooterFacebook";
+import IconFooterLinkedIn from "./Icons/IconFooterLinkedIn";
+import IconFooterInstagram from "./Icons/IconFooterInstagram";
 
 const Footer = () => {
   return (
-    <footer className="main_section bg-black  w-full">
-      <div className="flex flex-col md:flex-row justify-between items-start">
-        <Image
-          src={"/logo.svg"}
-          alt="Logo"
-          width={1000}
-          height={1000}
-          className="w-[250px] max-w-[400px] max-h-[200px]"
-        />
-        <div className=" flex flex-col md:flex-row gap-4 md:gap-24 xl:gap-52 mt-12 md:mt-0">
-          <div className="flex flex-col ">
-            <h5 className="uppercase text-white mb-4 md:mb-8">Produkty</h5>
-            <p className="text-white">Strop</p>
-            <p className="text-white">Strop</p>
-            <p className="text-white">StropStrop</p>
-          </div>
-          <div className="flex flex-col">
-            <h5 className="uppercase text-white mb-4 md:mb-8">Kontakt</h5>
-            <p className="text-white">+421903243393</p>
-            <p className="text-white">info@strop.sk</p>
+    <footer className=" bg-black  w-full">
+      <div className="main_section">
+        <div className="flex flex-col md:flex-row justify-between items-start">
+          <div className=" flex flex-col md:flex-row gap-4 md:gap-24 xl:gap-52 md:mt-0">
+            <div className="flex flex-col ">
+              <p className="text-white">
+                OC Avion, Ivánska cesta 16, 821 04 Bratislava
+              </p>
+              <p className="text-white">Pon - Pia 9:00 - 17:00</p>
+              <p className="text-white">Všeobecné obchodné podmienky</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-white">Domov</p>
+              <p className="text-white">Služby</p>
+              <p className="text-white">O nás</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-white">Pre profesionálov</p>
+              <p className="text-white">Blog</p>
+              <p className="text-white">Kontakt</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-white">ferrobeton@strop.sk</p>
+              <p className="text-white">0905 033 030, 0948 207 776</p>
+              <div className="flex flex-row gap-4 mt-2">
+                <IconFooterFacebook />
+                <IconFooterLinkedIn />
+                <IconFooterInstagram />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="mt-4 md:mt-12 border-white border-t">
-        <h6 className="text-white pt-4">GDPR</h6>
+      <div className="">
+        <Image
+          src={"/strop_pismo.svg"}
+          alt="strop pismo"
+          width={5000}
+          height={5000}
+          className="w-full h-full object-cover"
+        />
       </div>
     </footer>
   );
