db042ea3561e08875acc0af80a9d86919fde27c3
xkolumber <lubosk158@gmail.com>
2024-05-05 23:08:58 +0200

    panel options

--- src/app/Components/HomePageComponents/HomePagePanel.tsx
+++ src/app/Components/HomePageComponents/HomePagePanel.tsx
@@ -1,11 +1,46 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import ButtonElement from "../ButtonElement";
+import IconPlus from "../Icons/IconPlus";
+
+const panels = [
+  {
+    nazov: "Stropný panel 200",
+    popis1:
+      "Vďaka nízkej váhe a špeciálnemu spôsobu výroby sú dutinové panely vhodné aj pre tie najnáročnejšie projekty. Veľkou výhodou našich panelov je aj variabilita možností uloženia. Priestupy a otvory podľa individuálnych požiadaviek môžeme pripraviť už pri výrobe, vrátane otvorov pre schodiská a priestupy väčších rozmerov. Detaily a technické riešenia sú pripravuje naše oddelenie projekcie, takže  náš zákazník vopred vie, ako bude jeho riešenie vyzerať. ",
+    popis2:
+      "Dutinové panely s hrúbkou 200 mm sa vyrábajú v troch rôznych pevnostných kategóriách: základný model (200/1) zosilnený model (200/2) a najpevnejší panel (200/3).",
+  },
+  {
+    nazov: "Stropný panel 265",
+    popis1:
+      "Vďaka nízkej váhe 265 a špeciálnemu spôsobu výroby sú dutinové panely vhodné aj pre tie najnáročnejšie projekty. Veľkou výhodou našich panelov je aj variabilita možností uloženia. Priestupy a otvory podľa individuálnych požiadaviek môžeme pripraviť už pri výrobe, vrátane otvorov pre schodiská a priestupy väčších rozmerov. Detaily a technické riešenia sú pripravuje naše oddelenie projekcie, takže  náš zákazník vopred vie, ako bude jeho riešenie vyzerať. ",
+    popis2:
+      "Dutinové panely s hrúbkou 200 mm sa vyrábajú v troch rôznych pevnostných kategóriách: základný model (200/1) zosilnený model (200/2) a najpevnejší panel (200/3).",
+  },
+  {
+    nazov: "Stropný panel 320",
+    popis1:
+      "Vďaka nízkej váhe 320 a špeciálnemu spôsobu výroby sú dutinové panely vhodné aj pre tie najnáročnejšie projekty. Veľkou výhodou našich panelov je aj variabilita možností uloženia. Priestupy a otvory podľa individuálnych požiadaviek môžeme pripraviť už pri výrobe, vrátane otvorov pre schodiská a priestupy väčších rozmerov. Detaily a technické riešenia sú pripravuje naše oddelenie projekcie, takže  náš zákazník vopred vie, ako bude jeho riešenie vyzerať. ",
+    popis2:
+      "Dutinové panely s hrúbkou 200 mm sa vyrábajú v troch rôznych pevnostných kategóriách: základný model (200/1) zosilnený model (200/2) a najpevnejší panel (200/3).",
+  },
+  {
+    nazov: "Stropný panel 400",
+    popis1:
+      "Vďaka nízkej váhe 400 a špeciálnemu spôsobu výroby sú dutinové panely vhodné aj pre tie najnáročnejšie projekty. Veľkou výhodou našich panelov je aj variabilita možností uloženia. Priestupy a otvory podľa individuálnych požiadaviek môžeme pripraviť už pri výrobe, vrátane otvorov pre schodiská a priestupy väčších rozmerov. Detaily a technické riešenia sú pripravuje naše oddelenie projekcie, takže  náš zákazník vopred vie, ako bude jeho riešenie vyzerať. ",
+    popis2:
+      "Dutinové panely s hrúbkou 200 mm sa vyrábajú v troch rôznych pevnostných kategóriách: základný model (200/1) zosilnený model (200/2) a najpevnejší panel (200/3).",
+  },
+];
 
 const HomePagePanel = () => {
+  const [choosenIndex, setChoosenIndex] = useState(0);
+
   return (
-    <div className="main_section">
-      <div className="flex flex-col md:flex-row md:gap-6">
+    <div className="main_section ">
+      <div className="flex flex-col md:flex-row md:gap-6 ">
         <Image
           src={"/panel.jpg"}
           alt="panel"
@@ -13,28 +48,30 @@ const HomePagePanel = () => {
           height={500}
           className="w-full md:w-1/2"
         />
-        <div
-          className="
-        "
-        >
-          <p>[Produkty]</p>
-          <h2>Stropný panel</h2>
-          <p>
-            Vďaka nízkej váhe a špeciálnemu spôsobu výroby sú dutinové panely
-            vhodné aj pre tie najnáročnejšie projekty. Veľkou výhodou našich
-            panelov je aj variabilita možností uloženia. Priestupy a otvory
-            podľa individuálnych požiadaviek môžeme pripraviť už pri výrobe,
-            vrátane otvorov pre schodiská a priestupy väčších rozmerov. Detaily
-            a technické riešenia sú pripravuje naše oddelenie projekcie, takže
-            náš zákazník vopred vie, ako bude jeho riešenie vyzerať. Dutinové
-            panely s hrúbkou 200 mm sa vyrábajú v troch rôznych pevnostných
-            kategóriách: základný model (200/1) zosilnený model (200/2) a
-            najpevnejší panel (200/3).
-          </p>
-          <ButtonElement text="Zistiť viac" />
-          <p>Stropný panel</p>
-          <p>Stropný panel</p>
-          <p>Stropný panel</p>
+
+        <div className="justify-between flex flex-col">
+          <div className="flex flex-col">
+            <p>[Produkty]</p>
+            <h2>{panels[choosenIndex].nazov}</h2>
+            <p>{panels[choosenIndex].popis1}</p>
+            <p className="mt-4">{panels[choosenIndex].popis2}</p>
+            <ButtonElement text="Zistiť viac" />
+          </div>
+          <div className="flex flex-col">
+            {panels.map((panel, index) => (
+              <div
+                className="flex flex-row justify-between"
+                onClick={() => setChoosenIndex(index)}
+              >
+                {index != choosenIndex && (
+                  <>
+                    <div className="p">{panel.nazov}</div>
+                    <IconPlus />
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/app/Components/Icons/IconPlus.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const IconPlus = () => {
+  return (
+    <svg
+      width="36"
+      height="36"
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.4231 18.2692H12V17.4231H17.4231V12H18.2692V17.4231H23.6923V18.2692H18.2692V23.6923H17.4231V18.2692Z"
+        fill="black"
+      />
+      <circle cx="18" cy="18" r="17.5" stroke="black" />
+    </svg>
+  );
+};
+
+export default IconPlus;
