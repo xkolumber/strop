c15687aebe6c9a4cca4ce1cb211e2450c6214126
xkolumber <lubosk158@gmail.com>
2024-06-02 18:35:36 +0200

    show fetched zakazky on homepage

--- src/app/Components/HomePageComponents/HomePageBratislava.tsx
+++ src/app/Components/HomePageComponents/HomePageBratislava.tsx
@@ -1,50 +1,86 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import ButtonElement from "../ButtonElement";
-
-const zakazky = [
-  { title: "Senec - Shell" },
-  { title: "Miloslav" },
-  { title: "Šamorín" },
-  { title: "Čavoj" },
-  { title: "Nová Ves" },
-  { title: "Trnava" },
-  { title: "Púchov" },
-  { title: "Senica" },
-  { title: "Nitrianske Pravno" },
-  { title: "Veľký Biel" },
-  { title: "Piešťany" },
-  { title: "Cabaj Čápor" },
-  { title: "Nemšová" },
-  { title: "Lubina" },
-];
+import { PhotoCityDescription } from "@/app/firebase/interface";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { app } from "@/app/firebase/config";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const HomePageBratislava = () => {
+  const [zakazky, setZakazky] = useState<PhotoCityDescription[]>([]);
+
+  const [choosenCity, setChoosenCity] = useState("");
+  const [choosenDescription, setChoosenDescription] = useState("");
+  const [choosenPhoto, setChoosenPhoto] = useState("");
+
+  useEffect(() => {
+    const fetchZakazky = async () => {
+      const db = getFirestore(app);
+
+      try {
+        const panelyCollectionRef = collection(db, "stavby_popisy");
+        const querySnapshot = await getDocs(panelyCollectionRef);
+
+        const panelyProducts: PhotoCityDescription[] = querySnapshot.docs.map(
+          (doc) => ({
+            foto: doc.data().foto,
+            mesto: doc.data().mesto,
+            popis: doc.data().popis,
+          })
+        );
+
+        setZakazky(panelyProducts);
+        setChoosenCity(panelyProducts[0].mesto);
+        setChoosenDescription(panelyProducts[0].popis);
+        setChoosenPhoto(panelyProducts[0].foto);
+      } catch (error) {
+        console.error("Error fetching photos:", error);
+      }
+    };
+
+    fetchZakazky();
+  }, []);
+
+  const handleNewChoosenObject = (object: PhotoCityDescription) => {
+    setChoosenCity(object.mesto);
+    setChoosenDescription(object.popis);
+    setChoosenPhoto(object.foto);
+  };
   return (
     <div className="main_section flex flex-col md:flex-row bg-primary gap-6">
       <div className="flex flex-col md:w-1/2">
         <p>[ Naše projekty ]</p>
-        <h2>Bratislava - Ružinov</h2>
-        <p>
-          Máme za sebou mnoho komplikovaných stavebných projektov, vďaka ktorým
-          sme sa stali stabilným partnerom stavebných spoločností na území
-          celého Slovenska.
-        </p>
+        <h2>{choosenCity}</h2>
+        <p>{choosenDescription}</p>
 
         <div className="flex flex-wrap gap-8 mt-8 mb-8">
           {zakazky.map((object, index) => (
-            <ButtonElement text={object.title} key={index} />
+            <div
+              className=""
+              key={index}
+              onClick={() => handleNewChoosenObject(object)}
+            >
+              <ButtonElement text={object.mesto} />
+            </div>
           ))}
         </div>
       </div>
       <div className="flex flex-col md:w-1/2">
-        <Image
-          src={"/bratislava1.jpg"}
-          alt="panel"
-          width={500}
-          height={500}
-          className="w-full  h-full rounded-[8px]"
-        />{" "}
+        {zakazky ? (
+          <Image
+            src={choosenPhoto}
+            alt="panel"
+            width={500}
+            height={500}
+            className="w-full  rounded-[8px] object-cover h-[250px] md:h-[350px] 2xl:h-[400px] 3xl:h-[520px]"
+          />
+        ) : (
+          <div className="w-full">
+            <Skeleton height={476} borderRadius={8} baseColor="#c4c4c4" />
+          </div>
+        )}
         <div className="grid grid-cols-3 mt-8 gap-4 ">
           <Image
             src={"/bratislava1.jpg"}
