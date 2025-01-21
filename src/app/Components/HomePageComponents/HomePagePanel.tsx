abc7332a759c5cf429ff94b2e272d17df2dc4c1c
xkolumber <lubosk158@gmail.com>
2025-01-21 11:01:17 +0100

    homepage panel new photo

--- src/app/Components/HomePageComponents/HomePagePanel.tsx
+++ src/app/Components/HomePageComponents/HomePagePanel.tsx
@@ -22,11 +22,13 @@ const HomePagePanel = () => {
     <div className="main_section ">
       <div className="flex flex-col md:flex-row md:gap-6 xl:gap-8 2xl:gap-12 ">
         <Image
-          src="https://firebasestorage.googleapis.com/v0/b/strop-8bbc9.appspot.com/o/foto_web%2Fpanely_zoom.png?alt=media&token=ec112c63-b095-42b5-885e-4fe5f00bb6be"
+          src="https://firebasestorage.googleapis.com/v0/b/strop-8bbc9.appspot.com/o/foto_web%2Fstrop_min_2.png?alt=media&token=0fd1470b-44c6-4816-9b3f-680718613c66"
           alt="panel"
           width={800}
           height={800}
           className="w-full md:w-1/2 hidden md:block h-auto  object-cover rounded-[8px]"
+          placeholder="blur"
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAI0lEQVR4nGNwdbJesWC2v58/AxMDw///r89f2b9z99yM3AQAitAMJTwYjEYAAAAASUVORK5CYII="
         />
 
         <div className="justify-between flex flex-col md:w-1/2 ">
