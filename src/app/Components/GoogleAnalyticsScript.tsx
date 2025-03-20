277db58ed0e22b41d9edf3df17025591f59c4215
xkolumber <lubosk158@gmail.com>
2025-03-20 15:30:41 +0100

    goggle analytics

--- src/app/Components/GoogleAnalyticsScript.tsx
+++ src/app/Components/GoogleAnalyticsScript.tsx
@@ -6,23 +6,34 @@ const GoogleAnalyticsScript = () => {
     <>
       <Script
         id="my-script1"
+        strategy="afterInteractive"
         async
         src="https://www.googletagmanager.com/gtag/js?id=AW-16937406357"
       />
-      <Script id="my-script2">
-        {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'AW-16937406357');`}
+      <Script id="my-script2" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16937406357');
+        `}
       </Script>
 
-      <Script id="my-conversion-script">
-        {`gtag('event', 'conversion', {
-            'send_to': 'AW-16937406357/pEQoCNOKsqwaEJWfsYw_',
-            'value': 1.0,
-            'currency': 'EUR'
-          });`}
+      <Script id="my-conversion-script" strategy="afterInteractive">
+        {`
+          window.addEventListener('load', () => {
+            if (window.gtag) {
+              gtag('event', 'conversion', {
+                'send_to': 'AW-16937406357/pEQoCNOKsqwaEJWfsYw_',
+                'value': 1.0,
+                'currency': 'EUR'
+              });
+            } else {
+              console.error('gtag is not defined yet');
+            }
+          });
+        `}
       </Script>
     </>
   );
